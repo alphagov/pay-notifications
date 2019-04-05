15f5ae23b8032bdbdf1bf7a029dd02e55ab7be23
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-04-05 12:43:43 +0100

    PP-5068: Generate a selfsigned certificate on startup if necessary
    If we don't have a keypair available at startup, generate one.

--- src/docker-startup.sh
+++ src/docker-startup.sh
@@ -11,4 +11,14 @@ CONNECTOR_URL=${CONNECTOR_URL:-"https://localhost:5001"}
 sed -i "s,DD_CONNECTOR_URL,${DD_CONNECTOR_URL},g" $SITE_CONFIG
 sed -i "s,CONNECTOR_URL,${CONNECTOR_URL},g" $SITE_CONFIG
 
+# Generate a selfsigned key and certificate if we don't have one
+if [ ! -f /etc/keys/crt ]; then
+  dir=`mktemp -d`
+  openssl req -x509 -days 1000 -newkey rsa:2048 -nodes -subj '/CN=notifications' -keyout "$dir/key" -out "$dir/crt"
+  mkdir -p /etc/keys
+  install -m 0600 -o nginx -g nginx "$dir/key" /etc/keys/key
+  install -m 0644 -o nginx -g nginx "$dir/crt" /etc/keys/crt
+  rm -rf "$dir"
+fi
+
 nginx -g "daemon off;"
