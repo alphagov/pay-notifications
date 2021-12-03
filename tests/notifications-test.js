913a5b739db668dcd9cbcf5f0cd6c2f1d8954194
David Mays <david.mays@digital.cabinet-office.gov.uk>
2021-12-03 15:23:39 +0000

    [PP-8910] We now have a suite of working tests

--- tests/notifications-test.js
+++ tests/notifications-test.js
@@ -1,8 +1,18 @@
 // This is a really simple script to check the notifications image runs
 // correctly following a build of the Dockerfile and that it can respond
-// on the /healthcheck endpoint with an HTTP 200 response.
 
 const https = require('https')
+
+// This is the list of paths to test and the expected HTTP Status for each...
+const testsToRun = [
+    { path: '/healthcheck', expectedStatus: 200 },
+    { path: '/request-denied', expectedStatus: 400 },
+    { path: '/v1/api/notifications/epdq', expectedStatus: 200 },
+    { path: '/v1/api/notifications/smartpay', expectedStatus: 200 },
+    { path: '/v1/api/notifications/stripe', expectedStatus: 200 },
+    { path: '/v1/api/notifications/worldpay', expectedStatus: 200 },
+    { path: '/invalid/path', expectedStatus: 404 },
+]
 
 async function testNotificationPath(path, expectedStatus = 200) {
     return new Promise((resolve, reject) => {
@@ -19,9 +29,9 @@ async function testNotificationPath(path, expectedStatus = 200) {
 
         notificationReq.on("response", response => {
             if(response.statusCode == expectedStatus) {
-                resolve(`PASS: ${path} returned expected ${expectedStatus} Response`)
+                resolve(`PASS: \x1b[36m${path}\x1b[0m returned expected \x1b[36m${expectedStatus}\x1b[0m Response`)
             } else {
-                reject(`FAIL: ${path} returned status ${response.statusCode}, expected ${expectedStatus}`)
+                reject(`FAIL: \x1b[36m${path}\x1b[0m returned status \x1b[31m${response.statusCode}\x1b[0m, expected \x1b[36m${expectedStatus}\x1b[0m`)
             }
         })
 
@@ -30,26 +40,16 @@ async function testNotificationPath(path, expectedStatus = 200) {
         notificationReq.on("error", error => {
             reject(error)
         })
-
-    }).catch(error => {
-        console.error(error)
     })
 }
 
+// This loops through the list of `testsToRun` above...
 async function runTests() {
     const failedTests = []
-    const testsToRun = [
-        testNotificationPath('/healthcheck'),
-        testNotificationPath('/request-denied', 400),
-        testNotificationPath('/v1/api/notifications/worldpay'),
-        testNotificationPath('/v1/api/notifications/epdq'),
-        testNotificationPath('/v1/api/notifications/stripe'),
-        testNotificationPath('/invalid/path', 404)
-    ]
 
     for(const thisTest of testsToRun) {
         try {
-            const thisTestResult = await thisTest
+            const thisTestResult = await testNotificationPath(thisTest.path, thisTest.expectedStatus)
             console.log(thisTestResult)
         } catch (error) {
             console.error(error)
@@ -61,11 +61,20 @@ async function runTests() {
 }
 
 async function start() {
+    console.log(`Starting Notification Container Tests...`)
     const failedTests = await runTests()
     if(failedTests.length > 0) {
-        console.error(`There were ${failedTests.length} failed tests`)
-        console.error(failedTests)
+        console.error(`There were ${failedTests.length} failed tests:`)
+        console.group()
+        failedTests.forEach((thisFailedTest, thisFailedTestNum) => {
+            console.error(`#${thisFailedTestNum+1}: ${thisFailedTest}`)
+        })
+        console.groupEnd()
+        console.error('\x1b[31m%s\x1b[0m', "One or more tests failed.")
         process.exit(1)
+    }
+    else {
+        console.log('\x1b[32m%s\x1b[0m', "All tests passed successfully.")
     }
 }
 
