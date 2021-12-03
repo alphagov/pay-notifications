8d766f7c2ea225757cd6ff5473470242991c5c8a
David Mays <david.mays@digital.cabinet-office.gov.uk>
2021-12-03 15:58:07 +0000

    [PP-8910] Break out GHA Steps into Workflow YAML.
    The GHA Log viewer seems to be glitching-out and losing the log after
    the first 50 lines. Let's see if this fixes it.

--- tests/docker-test.sh
+++ tests/docker-test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Running tests"
+echo "Building Docker Compose Stack for Notifications Test..."
+docker-compose build
 
-echo "Starting containers"
-
-if docker-compose up --build --force-recreate --abort-on-container-exit --exit-code-from tester tester; then
+echo "Starting 'tester' container and its depdendencies..."
+if docker-compose up --abort-on-container-exit --exit-code-from tester tester; then
   echo "Pass"
 else
   echo "Fail"
